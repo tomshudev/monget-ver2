2a3629df1d3387ba7674edf5763f5504bfd27d52
Tom Shushan <tomsh@axiom.security>
2023-05-28 10:36:11 +0300

    fix

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -1,57 +1,59 @@
-import { enumType, extendType, nonNull, objectType, stringArg } from 'nexus'
-import { Habit } from './Habit'
+// import { enumType, extendType, nonNull, objectType, stringArg } from 'nexus'
+// import { Habit } from './Habit'
 
-export const User = objectType({
-  name: 'User',
-  definition(t) {
-    t.string('id')
-    t.string('name')
-    t.string('email')
-    t.string('image')
-    t.field('role', { type: Role })
-    t.list.field('habits', {
-      type: Habit,
-      async resolve(parent, _args, ctx) {
-        return await ctx.prisma.habit.findMany({
-          where: {
-            userId: parent.id!,
-          },
-        })
-      },
-    })
-  },
-})
+// export const User = objectType({
+//   name: 'User',
+//   definition(t) {
+//     t.string('id')
+//     t.string('name')
+//     t.string('email')
+//     t.string('image')
+//     t.field('role', { type: Role })
+//     t.list.field('habits', {
+//       type: Habit,
+//       async resolve(parent, _args, ctx) {
+//         return await ctx.prisma.habit.findMany({
+//           where: {
+//             userId: parent.id!,
+//           },
+//         })
+//       },
+//     })
+//   },
+// })
 
-const Role = enumType({
-  name: 'Role',
-  members: ['USER', 'ADMIN'],
-})
+// const Role = enumType({
+//   name: 'Role',
+//   members: ['USER', 'ADMIN'],
+// })
 
-export const UsersQuery = extendType({
-  type: 'Query',
-  definition(t) {
-    t.nonNull.list.field('users', {
-      type: 'User',
-      resolve(_parennt, _args, ctx) {
-        return ctx.prisma.user.findMany()
-      },
-    })
-  },
-})
+// export const UsersQuery = extendType({
+//   type: 'Query',
+//   definition(t) {
+//     t.nonNull.list.field('users', {
+//       type: 'User',
+//       resolve(_parennt, _args, ctx) {
+//         return ctx.prisma.user.findMany()
+//       },
+//     })
+//   },
+// })
 
-export const UserQuery = extendType({
-  type: 'Query',
-  definition(t) {
-    t.field('user', {
-      type: 'User',
-      args: { userId: nonNull(stringArg()) },
-      resolve(_parennt, args, ctx) {
-        return ctx.prisma.user.findUnique({
-          where: {
-            id: args.userId,
-          },
-        })
-      },
-    })
-  },
-})
+// export const UserQuery = extendType({
+//   type: 'Query',
+//   definition(t) {
+//     t.field('user', {
+//       type: 'User',
+//       args: { userId: nonNull(stringArg()) },
+//       resolve(_parennt, args, ctx) {
+//         return ctx.prisma.user.findUnique({
+//           where: {
+//             id: args.userId,
+//           },
+//         })
+//       },
+//     })
+//   },
+// })
+
+export {}
