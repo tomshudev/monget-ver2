a1aa248350f78d7093c05b25025396068df4e25a
Tom Shushan <tomsh@axiom.security>
2023-06-30 13:25:03 +0300

    Use remote server

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/api/graphql',
+  // uri: 'http://localhost:4000/api/graphql',
+  uri: 'https://habits-server-pimf.onrender.com/api/graphql',
   cache: new InMemoryCache(),
 })
 
